90fb2b6738de94d5488f230181214685d8afa969
samoyedic <643976404@qq.com>
2018-12-07 17:16:44 +0800

    jugle up

--- python_learn/chapter_cycle.py
+++ python_learn/chapter_cycle.py
@@ -5,3 +5,18 @@
 # @Site    : 
 # @File    : chapter_cycle.py
 # @Software: PyCharm
+"""
+循环 for while
+"""
+lists = [1, 2, 3, 4, 5]
+for x in lists:
+    print(x)
+
+flag = int(input("enter something here"))
+while flag < 10:
+    print('the number less than ten')
+else:
+    print('the number eq or more than ten')
+
+for x in range(1, 10, 3):
+    print(x)
--- python_learn/chapter_judge.py
+++ python_learn/chapter_judge.py
@@ -6,5 +6,18 @@
 # @File    : chapter_judge.py
 # @Software: PyCharm
 """
-条件判断
+条件判断 if elif else  == >= <= != > <
 """
+flag = int(1)
+if flag == 1 :
+    print('flag values is 1')
+else:
+    print('flag values is not 1')
+
+keywords = int(input('enter number'))
+if keywords < 10:
+    print('the keywords less than 10')
+elif keywords > 10 & keywords < 100:
+    print('the keywords is more than 10 and less than 100')
+else:
+    print('the keywords is eq 10 or more than 100')
